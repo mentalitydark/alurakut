545c9461b1779b844788f4d6c7ef37052572f51f
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-07-12 11:27:15 -0300

    Inicio do Alurakut

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import {AlurakutStyles} from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  * {
+    padding: 0px;
+    margin: 0px;
     box-sizing: border-box;
   }
+  body {
+    background-color: #D9E6F6;
+    font-family: sans-serif
+  }
+  #__next {
+    height: 100vh
+  }
+  img {
+    max-width:100%;
+    height: auto;
+    display: block;
+  }
+
+  ${AlurakutStyles}
 `
 
 const theme = {
