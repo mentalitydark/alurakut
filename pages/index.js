d328e60c36c26f624421d1dc9d87d953acef389e
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-07-13 22:17:10 -0300

    Comunidades Update;

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ function ProfileRelationsAreaBox(props) {
                 {props.boxTitle} ({(props.boxDados).length})
             </h2>
             <ul>
-                {console.log((props.boxDados).slice(0,6))}
                 {(props.boxDados).slice(0,6).map((dado) => {
                     return (
                         <li key={(props.boxDados).id}>
@@ -45,7 +44,16 @@ function ProfileRelationsAreaBox(props) {
 
 export default function Home() {
     const githubUser = "mentalitydark";
-    const [comunidades, setComunidades] = React.useState([{id: new Date().toISOString(), title: "Eu odeio acordar cedo!", image: "https://alurakut.vercel.app/capa-comunidade-01.jpg"}]);
+    const [comunidades, setComunidades] = React.useState([
+        {id: new Date().toISOString(), title: "Eu odeio acordar cedo!", image: "https://alurakut.vercel.app/capa-comunidade-01.jpg"},
+        {id: new Date().toISOString(), title: "Minecraft", image: "https://images-na.ssl-images-amazon.com/images/I/418cEZfh8-L.jpg"},
+        {id: new Date().toISOString(), title: "Borderlands 3", image: "https://image.api.playstation.com/vulcan/ap/rnd/202010/2323/p50N4PBK9rNanGYKFecTvac5.png"},
+        {id: new Date().toISOString(), title: "Steam", image: "https://tecnoblog.net/wp-content/uploads/2018/12/steam-logo-700x394.jpg"},
+        {id: new Date().toISOString(), title: "O negócio da família", image: "https://lh3.googleusercontent.com/proxy/bNXJgPRS_l79EoVecIJO1td36cHw2Op3eL6tgJI8dAeTPbzzOOlwW_VVdOH9rSfKVmrK-3flmaIwm8RbwS0LuXOV8pcQpFvmVNkdBgM_Qa3Xyv3nIYq1SsJpLlqCXOeTH5Q"},
+        {id: new Date().toISOString(), title: "Dark Souls", image: "https://criticalhits.com.br/wp-content/uploads/2020/12/cb109d5d1402ae804422a89aa168da00.jpg"},
+    ]);
+    
+    
     const pessoasFavoritas = [{id: 1,title: 'juunegreiros', image: 'https://github.com/juunegreiros.png'}, 
                             {id: 2,title: 'omariosouto', image: 'https://github.com/omariosouto.png'},
                             {id: 3,title: 'peas', image: 'https://github.com/peas.png'}, 
