daf893b60f5f9915eb3144f3b88bdf1f98a0ea7e
Guilherme Oliveira <36698608+mentalitydark@users.noreply.github.com>
2021-07-14 11:30:36 -0300

    New Files; Add constants.js, CardBox

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,24 @@
 import React from 'react';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
-
-function ProfileSideBar(props) {
-    return (
-        <Box as="aside">
-            <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: "8px"}}/>
-            <hr/>
-            <p>
-                <a className="boxLink" href={`https://github.com/${props.githubUser}`}>@{props.githubUser} </a>
-            </p>
-            <hr/>
-            <AlurakutProfileSidebarMenuDefault/>
-        </Box>
-    );
-}
-function ProfileRelationsAreaBox(props) {
-    return (
-        <ProfileRelationsBoxWrapper>
-            <h2 className="smallTitle">
-                {props.boxTitle} ({(props.boxDados).length})
-            </h2>
-            <ul>
-                {(props.boxDados).slice(0,6).map((dado) => {
-                    return (
-                        <li key={(props.boxDados).id}>
-                            <a href={`/${dado.type}/${dado.title}`}>
-                                <img src={dado.image}/>
-                                <span>{dado.title}</span>
-                            </a>
-                        </li>
-                    );
-                })}
-            </ul>
-        </ProfileRelationsBoxWrapper>
-    )
-}
-
-
-
+import ProfileSideBar from '../src/components/ProfileSideBar';
+import CardBox from '../src/components/CardBox';
+import {User, INITIAL_COMMUNITIES, INITIAL_FAVORITES, myInfos, myReactions} from '../src/utils/constants';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 
 export default function Home() {
-    const githubUser = "mentalitydark";
-    const [comunidades, setComunidades] = React.useState([
-        {id: new Date().toISOString(), title: "Eu odeio acordar cedo!", image: "https://alurakut.vercel.app/capa-comunidade-01.jpg"},
-        {id: new Date().toISOString(), title: "Minecraft", image: "https://images-na.ssl-images-amazon.com/images/I/418cEZfh8-L.jpg"},
-        {id: new Date().toISOString(), title: "Borderlands 3", image: "https://image.api.playstation.com/vulcan/ap/rnd/202010/2323/p50N4PBK9rNanGYKFecTvac5.png"},
-        {id: new Date().toISOString(), title: "Steam", image: "https://tecnoblog.net/wp-content/uploads/2018/12/steam-logo-700x394.jpg"},
-        {id: new Date().toISOString(), title: "O negócio da família", image: "https://lh3.googleusercontent.com/proxy/bNXJgPRS_l79EoVecIJO1td36cHw2Op3eL6tgJI8dAeTPbzzOOlwW_VVdOH9rSfKVmrK-3flmaIwm8RbwS0LuXOV8pcQpFvmVNkdBgM_Qa3Xyv3nIYq1SsJpLlqCXOeTH5Q"},
-        {id: new Date().toISOString(), title: "Dark Souls", image: "https://criticalhits.com.br/wp-content/uploads/2020/12/cb109d5d1402ae804422a89aa168da00.jpg"},
-    ]);
-    
-    
-    const pessoasFavoritas = [{id: 1,title: 'juunegreiros', image: 'https://github.com/juunegreiros.png'}, 
-                            {id: 2,title: 'omariosouto', image: 'https://github.com/omariosouto.png'},
-                            {id: 3,title: 'peas', image: 'https://github.com/peas.png'}, 
-                            {id: 4,title: 'rafaballerini', image: 'https://github.com/rafaballerini.png'}, 
-                            {id: 5,title: 'marcobrunodev', image: 'https://github.com/marcobrunodev.png'},
-                            {id: 6,title: 'felipefialho', image: 'https://github.com/felipefialho.png'}];
-    const myInfos = [{ name: 'Recados', slug: '10', icon: 'book' },
-                    { name: 'Fotos', slug: '1', icon: 'camera' },
-                    { name: 'Videos', slug: '0', icon: 'video-camera' },
-                    { name: 'Fãs', slug: '1', icon: 'star' },
-                    { name: 'Mensagens', slug: '1', icon: 'email' }];
-    const reactions = [{ name: 'Confiável', slug: '3', icon: 'smile' },
-                    { name: 'Legal', slug: '2', icon: 'cool' },
-                    { name: 'Sexy', slug: '0', icon: 'heart' }];
-  return (
+    const [comunidades, setComunidades] = React.useState(INITIAL_COMMUNITIES);
+  return ( /* html */
     <>
-        <AlurakutMenu githubUser={githubUser}/>
+        <AlurakutMenu githubUser={User}/>
         <MainGrid>
         <div className="profileArea" style={{gridArea: 'profileArea'}}>
-            <ProfileSideBar githubUser={githubUser}/>
+            <ProfileSideBar githubUser={User}/>
         </div>
         <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
             <Box>
                 <h1 className="title">Bem-vindo(a)</h1>
-                <OrkutNostalgicIconSet myInfos={myInfos} reactions={reactions}/>
+                <OrkutNostalgicIconSet myInfos={myInfos} reactions={myReactions}/>
             </Box>
             <Box>
                 <h2 className="subtitle">O que você deseja fazer?</h2>
@@ -111,9 +51,8 @@ export default function Home() {
             </Box>
         </div>
         <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
-            {console.log(comunidades)}
-            <ProfileRelationsAreaBox boxTitle="Comunidade" boxDados={comunidades} type="comunidade"/>
-            <ProfileRelationsAreaBox boxTitle="Pessoas Favoritas" boxDados={pessoasFavoritas} type="user"/>
+            <CardBox boxTitle="Comunidade" boxDados={comunidades} type="comunidade"/>
+            <CardBox boxTitle="Pessoas Favoritas" boxDados={INITIAL_FAVORITES} type="user"/>
         </div>
         </MainGrid>
     </>
